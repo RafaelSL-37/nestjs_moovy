a5f0472c7c280e7d5aa4992edd2857a7787019ae
RafaelSL <rafaelsilvadelima37@gmail.com>
2023-06-23 19:38:59 -0300

    adding openai api

--- src/api/chatgpt/chatgpt.api.ts
+++ src/api/chatgpt/chatgpt.api.ts
@@ -1,2 +1,14 @@
 //TODO: CREATE CHATGPT MOPVIE SUGGESTION API
 //SUGGESTIONS MUST BE COMPARED AGAINST EACH OTHER (FROM DIFFERENT APIS)
+const OpenAI = require("openai-api");
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+//TODO: REMOVE TYPE ANY
+const gptResponse = async (prompt: string): Promise<any> => {
+  const gptResponse = await openai.complete({
+    engine: "davinci",
+    prompt: prompt,
+  });
+
+  console.log(gptResponse.data);
+};
