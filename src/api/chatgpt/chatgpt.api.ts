a59d33dfb9cd79c2ff116fe5ef087708265ca03d
RafaelSL <rafaelsilvadelima37@gmail.com>
2023-07-10 20:51:27 -0300

    adding prompt and treatment

--- src/api/chatgpt/chatgpt.api.ts
+++ src/api/chatgpt/chatgpt.api.ts
@@ -4,11 +4,38 @@ const OpenAI = require("openai-api");
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 //TODO: REMOVE TYPE ANY
-const gptResponse = async (prompt: string): Promise<any> => {
+const gptResponse = async (
+  goodMovies: string[],
+  badMovies: string[]
+): Promise<any> => {
+  const goodMoviesList = goodMovies.reduce(
+    (list, goodMovie, index) => `${list}
+  ${index} - ${goodMovie}`
+  );
+
+  const badMoviesList = badMovies.reduce(
+    (list, badMovie, index) => `${list}
+  ${index} - ${badMovie}`
+  );
+
+  const prompt = `Me dê 5 sugestões de filmes baseados nos filmes que irei informar a seguir.
+
+  ${goodMoviesList}
+  
+  Quero que evite filmes similares a esses:
+  
+  ${badMoviesList}
+  
+  O título dos filmes deve ser em inglês. A resposta deve ser um array de strings em JSON. Nada mais deve ser adicionado a resposta.`;
+
   const gptResponse = await openai.complete({
     engine: "davinci",
     prompt: prompt,
+    max_tokens: 250,
+    temperature: 0.2,
   });
 
   console.log(gptResponse.data);
+
+  return gptResponse.data;
 };
